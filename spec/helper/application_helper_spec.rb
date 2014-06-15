066698c7be6f06303fbc0064e69baa277e977bab
Pastelblush <pastelblush@gmail.com>
2014-06-15 19:30:32 +0800

    Make a basic User model (including secure passwords) and exercise

--- spec/helper/application_helper_spec.rb
+++ spec/helper/application_helper_spec.rb
@@ -3,10 +3,16 @@ require 'spec_helper'
 describe ApplicationHelper do
 
   describe "full_title" do
-    it {should expect(full_title("foo")).to match(/foo/)}
+    it "should include the page title" do
+      expect(full_title("foo")).to match(/foo/)
+    end
 
-    it {should expect(full_title("foo")).to match(/^Ruby on Rails Tutorial Sample App/)}
+    it "should include the base title" do
+      expect(full_title("foo")).to match(/^Ruby on Rails Tutorial Sample App/)
+    end
 
-    it {should expect(full_title("")).not_to match(/\|/)}
+    it "should not include a bar for the home page" do
+      expect(full_title("")).not_to match(/\|/)
+    end
   end
 end
