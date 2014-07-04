06499befa5c3ba83c3d60ead7b962caca719f40c
Pastelblush <pastelblush@gmail.com>
2014-07-04 13:07:59 +0800

    Finish sign in and exercise

---
+++ spec/support/have_success_message.rb
@@ -0,0 +1,5 @@
+RSpec::Matchers.define :have_success_message do |message|
+  match do |page|
+    expect(page).to have_selector('div.alert.alert-success', text: message)
+  end
+end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,1 +1,15 @@
 include ApplicationHelper
+
+def valid_signin(user)
+  fill_in "Email",    with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+end
+
+
+
+RSpec::Matchers.define :not_have_error_message do |message|
+  match do |page|
+  	expect(page).to have_selector('div.alert.alert-error', text: message)
+  end
+end
